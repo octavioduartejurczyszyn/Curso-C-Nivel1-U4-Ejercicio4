6737d581307a04a83f19e7bae803dab22e30a43a
octavioduartejurczyszyn <tatoduarte9@gmail.com>
2024-02-20 13:25:41 +0100

    Primera subida

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un programa para ingresar tres números y emitir un cartel
+            // aclaratorio si la suma de los dos primeros es mayor al producto del segundo con el tercero.
+            int n1, n2, n3;
+            Console.WriteLine("Escriba un nro:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba otro:");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba el último:");
+            n3 = int.Parse(Console.ReadLine());
+                if((n1 + n2) > (n2 * n3))
+                Console.WriteLine("Sí, es mayor");
+
+        }
+    }
+}
